5e45684ad2bc709eadb6ff262edffac42dbf9abf
thevarunkhatri <varunmk05@gmail.com>
2020-09-20 14:01:03 -0400

    sanity dashboard updated

---
+++ studio/src/dashboardConfig.js
@@ -0,0 +1,30 @@
+export default {
+    widgets: [
+        {
+            name: 'project-info'
+        },
+        {
+            name: 'project-users'
+        },
+        {
+            name: 'netlify',
+            options: {
+                title: 'My Netlify deploys',
+                sites: [
+                    {
+                    title: 'Sanity Studio',
+                    apiId: 'bba80faf-3c85-444d-a216-3dd8b9bf67d7',
+                    buildHookId: '5f6794ff6ac815a7733ec386',
+                    name: 'epic-austin-37639b',
+                    },
+                    {
+                    title: 'Website',
+                    apiId: '7086e1e3-57b8-43b7-83a0-d3b09e6fe1da',
+                    buildHookId: '5f6795208466db1d0809de98',
+                    name: 'competent-montalcini-73a67e'
+                    }
+                ]
+            }
+        }
+    ]
+}
